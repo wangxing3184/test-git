01b1f0bc8e3c9b9fb80cbc4584757f2df9353f7e
wangxing3184 <xing.wang@witintech.com>
2023-12-06 16:03:58 +0800

    test

---
+++ EQ/eq_process.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <math.h>
+#include "eq_process.h"
+
+void initializeEQ(EQ *eq, float frequency, float gain, float qFactor) {
+	eq->frequency = frequency;
+	eq->gain = pow(10,gain/20);
+	eq->qFactor = qFactor;
+        eq->w0 = M_PI *  eq->frequency/8000.0;
+	eq->alpha = sin(eq->w0) / (2 * eq->qFactor);
+	eq->a0 = 1 + eq->alpha / eq->gain;
+
+	eq->b0 = (1 + eq->alpha * eq->gain) / eq->a0;
+	eq->b1 = (-2 * cos(eq->w0)) / eq->a0;
+	eq->b2 = (1 - eq->alpha * eq->gain) / eq->a0;
+	eq->a1 = (-2 * cos(eq->w0)) / eq->a0;;
+	eq->a2 = (1 - eq->alpha / eq->gain) / eq->a0;
+}
+
+
+float calculateOutput(EQ *eq, float input) {
+
+
+	float output = eq->b0 * input + eq->b1 * eq->x1 + eq->b2 * eq->x2 - eq->a1 * eq->y1 - eq->a2 * eq->y2;
+
+	eq->x2 = eq->x1;
+	eq->x1 = input;
+	eq->y2 = eq->y1;
+	eq->y1 = output;
+
+	return output;
+}
---
+++ EQ/eq_process.h
@@ -0,0 +1,29 @@
+#ifndef  __EQ_PROCESS_H__
+#define  __EQ_PROCESS_H__
+
+
+typedef struct {
+	float frequency;
+	float gain;
+	float qFactor;
+        float x1;
+        float x2;
+        float y1;
+        float y2;
+
+        float w0 ;
+	float alpha;
+	float a0 ;
+	float a1 ;
+	float a2 ;
+	float b0 ;
+	float b1;
+	float b2 ;
+
+} EQ;
+
+void initializeEQ(EQ *eq, float frequency, float gain, float qFactor);
+
+float calculateOutput(EQ *eq, float input);
+
+#endif
